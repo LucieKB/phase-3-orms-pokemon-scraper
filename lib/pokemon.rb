e2c523e9eb3ed21e223a441c25571b20014d630d
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-07-11 12:40:56 +0200

    commit tree 1a6ab63cfff2e8148f746e71e4db10a5993a3066

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
-class Pokemon
+class Pokemon 
+
+    attr_accessor :name, :type, :id
+
+    def initialize(name:, type:, id:nil)
+        @id = id
+        @name = name
+        @type = type
+    end
+
+    def save
+        sql = <<- SQL
+        INSERT INTO pokemons (name, type)
+        VALUES (?,?)
+        SQL
+
+        DB[conn:].execute(sql, self.name, self.type)
+        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemons")[0][0]
+        self
+    end
 end
